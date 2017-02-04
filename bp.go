a0e6c47ceaef849d9041dba637011f21f01fc387
peterzky <378096232@qq.com>
2017-02-04 18:59:38 +0800

    restructure

--- bp.go
+++ bp.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"fmt"
-	"./gonn"
+
+	"./lib/gonn"
 )
 
-func main(){
-	nn := gonn.NewNetwork(3,4,3,true,0.000001,0.0000001)
+func main() {
+	nn := gonn.NewNetwork(3, 4, 3, true, 0.000001, 0.0000001)
 	inputs := make([][]float64, 15)
-	targets := make([][]float64,15)
+	targets := make([][]float64, 15)
 
-	for i := 0; i < len(inputs);i++ {
+	for i := 0; i < len(inputs); i++ {
 		x := float64(i)
-		inputs[i] = []float64{x,x+1,x+2}
-		targets[i] = []float64{x*(x+1),(x+1)*(x+2),x+3}
+		inputs[i] = []float64{x, x + 1, x + 2}
+		targets[i] = []float64{x * (x + 1), (x + 1) * (x + 2), x + 3}
 	}
-	fmt.Println(inputs,targets)
+	fmt.Println(inputs, targets)
 
-	
+	nn.Train(inputs, targets, 10000000)
 
-	nn.Train(inputs,targets,10000000)
-
-	for _,p := range inputs{
+	for _, p := range inputs {
 		fmt.Println(nn.Forward(p))
 	}
 
@@ -30,6 +29,6 @@ func main(){
 	// test := nn.Forward(testcase)
 
 	// fmt.Println(test)
-	gonn.DumpNN("dump.json",nn)
+	gonn.DumpNN("./dump/dump.json", nn)
 
 }
