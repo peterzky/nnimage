efa7ca648d6431b23e5c1141e6b9b36c398e285a
peterzky <378096232@qq.com>
2017-02-13 14:39:15 +0800

    generate color image

--- testgen.go
+++ testgen.go
@@ -1,10 +1,38 @@
 package main
 
+import "github.com/peterzky/nnimage/base"
 import (
-	"github.com/peterzky/nnimage/base"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
+// genColor ...
+func genColor(intr int) [][]uint8 {
+	output := [][]uint8{}
+	for i := 0; i <= 255; i += intr {
+		for j := 0; j <= 255; j += intr {
+			for k := 0; k <= 255; k += intr {
+				a := []uint8{uint8(i), uint8(j), uint8(k)}
+				output = append(output, a)
+
+			}
+		}
+	}
+	return output
+
+}
+
 func main() {
-	color := []uint8{16, 32, 64}
-	base.Genimage(color)
+	intr, err := strconv.ParseInt(os.Args[1], 10, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	colors := genColor(int(intr))
+	for _, v := range colors {
+		base.Genimage(v)
+		fmt.Println(v)
+	}
+
 }
