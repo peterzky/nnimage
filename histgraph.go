a6282fbe223ac0eae2b08e227ccb3434cb5ba91d
peterzky <378096232@qq.com>
2017-03-20 15:42:09 +0800

    output csv file

--- histgraph.go
+++ histgraph.go
@@ -1,26 +1,44 @@
 package main
 
 import "github.com/peterzky/nnimage/base"
-import "github.com/codahale/hdrhistogram"
-import "fmt"
+import "encoding/csv"
+import "os"
+import "log"
+import "strconv"
+
+func parsePixel(p [][]float64) (out [][]string) {
+	for _, x := range p {
+		r := strconv.FormatFloat(x[0], 'f', 0, 64)
+		g := strconv.FormatFloat(x[1], 'f', 0, 64)
+		b := strconv.FormatFloat(x[2], 'f', 0, 64)
+		out = append(out, []string{r, g, b})
+	}
+	return out
+
+}
 
 func main() {
-	m := base.ImageToPixels("./sample/00ff00.png")
-	p := base.ConcatPixels(m)
-	var a []int64
-	for _, v := range p {
-		a = append(a, int64(v[2]))
-	}
-	hist := hdrhistogram.New(0, 255, 1)
-	for _, sample := range a {
-		hist.RecordValue(sample)
-	}
-	fmt.Println(hist.Mean())
-	fmt.Println(hist.Distribution())
-	fmt.Println(len(a))
-	// cod := []string{"b7", "c6", "d3", "e5", "f4", "ef", "e5", "d5", "ff", "88"}
-	// for _, v := range cod {
-	// 	fmt.Println(hex.DecodeString(v))
-	// }
+	m := base.ImageToPixels("/home/peterzky/Downloads/downloads/一数科技/手/hand2.jpg")
+	records := parsePixel(base.ConcatPixels(m))
 
+	out_f, err := os.OpenFile("distro.csv", os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		panic("error!")
+	}
+
+	defer out_f.Close()
+	w := csv.NewWriter(out_f)
+
+	for i, record := range records {
+		if i < 1000 {
+			if err := w.Write(record); err != nil {
+				log.Fatal("Error writing data")
+			}
+		}
+	}
+	w.Flush()
+
+	if err := w.Error(); err != nil {
+		log.Fatal(err)
+	}
 }
